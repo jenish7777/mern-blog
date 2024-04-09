9e43bd9dd9ae27bbff1bcfe5917bbd9df691f218
Zenish Sangani <jenishsangani54@gmail.com>
2024-04-09 19:00:27 +0530

    create the hander section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,17 @@ import Dashboard from "./pages/Dashboard";
 import Project from "./pages/Project";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Header from "./componets/Header";
 
 export default function App(){
   return(
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/about" element={<About/>}/>
       <Route path="/dashboard" element={<Dashboard/>}/>
-      <Route path="/project" element={<Project/>}/>
+      <Route path="/projects" element={<Project/>}/>
       <Route path="/sign-in" element={<SignIn/>}/>
       <Route path="/sign-up" element={<SignUp/>}/>
     </Routes>
