8a6d2d54ec7e43916dfdb5800830412335675c4e
Zenish Sangani <jenishsangani54@gmail.com>
2024-04-10 09:35:31 +0530

    connect to the data base

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config();
+
+mongoose.connect("mongodb+srv://jenishsangani54:abcd@mern-blog.lvaogjb.mongodb.net/mern-blog?retryWrites=true&w=majority&appName=mern-blog").then(
+    ()=>{
+        console.log("Database is Connected")
+    }
+).catch((err)=>{
+    console.log(err)
+})
 
 const app=express();
 
