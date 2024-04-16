f161ac9f1da23cf3e5f6e35d01afd8eaf75cd249
Zenish Sangani <jenishsangani54@gmail.com>
2024-04-17 00:54:25 +0530

    add like functionlity to comment

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -34,3 +34,26 @@ export const getPostComment =async (req, res, next) => {
     }
 
 }
+
+export const likeComment =async (req, res, next) => {
+    try {
+        const comment =await Comment.findById(req.params.commentId);
+        if(!comment){
+            return next(errorHandler(404,"Comment not found"))
+        }
+        const userIndex = comment.likes.indexOf(req.user.id);
+
+        if(userIndex === -1){
+            comment.numberOfLikes =+ 1
+            comment.likes.push(req.user.id);
+        
+        }else{
+            comment.numberOfLikes -= 1;
+            comment.likes.splice(userIndex,1);
+        }
+        await comment.save();
+        res.status(200).json(comment);
+    } catch (error) {
+        next(error)
+    }
+}
