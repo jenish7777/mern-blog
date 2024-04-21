d4f3362495258389901d36fd6ccc1c7133134fde
Zenish Sangani <jenishsangani54@gmail.com>
2024-04-21 18:11:40 +0530

    complete about and project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className='text-3xl font-semibold text-center my-7'>About Zenish's  Blog</h1>
+          <div className=" text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Zenish' Blog is a personal blog that I created to share my
+              knowledge and experience with the world of web development.
+              I hope you enjoy teading my blog. If you have any questions or
+            </p>
+
+            <p>
+            When blogs first emerged around 1994, people used them mainly to share their personal lives, including their experiences and interests. Now, blogs have become an essential online marketing strategy for businesses and one of the most profitable career choices for individuals.
+            </p>
+
+            <p>
+            On the other hand, Natasha Kravchuk shares recipes and tutorials on using specific kitchen tools on her food blog, Natasha's Kitchen. Her blog is a go-to resource for recipes, including holiday and vegetarian options.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import  CallToAction from "../componets/CallToAction"
 
 export default function Project() {
   return (
-    <div>
-      Project
+    <div className='min-h-scrren max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+      <h1 className='text-3xl font-semibold'>Projecs</h1>
+      <p className='text-md text-gray-500'>Build fun and engaing projecs while learning HTML, CSS and JavaScript!</p>
+      <CallToAction/>
     </div>
   )
 }
