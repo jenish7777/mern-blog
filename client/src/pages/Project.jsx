c8da3f89f6e5af60e97889bcf8c1e412f633305b
Zenish Sangani <jenishsangani54@gmail.com>
2025-01-30 15:32:10 +0530

    updaate project section

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import  CallToAction from "../componets/CallToAction"
+import CallToAction from "../componets/CallToAction"
 
 export default function Project() {
   return (
-    <div className='min-h-scrren max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
       <h1 className='text-3xl font-semibold'>Projects</h1>
       <p className='text-md text-gray-500'>Build fun and engaing projecs while learning HTML, CSS and JavaScript!</p>
       <CallToAction/>
